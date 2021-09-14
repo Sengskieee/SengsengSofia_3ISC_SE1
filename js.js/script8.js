82e495e2161a97b7cca3d0038e41da32f31cfb73
Sengskieee <sofiaallyson.sengseng.iics@ust.edu.ph>
2021-09-14 09:56:08 +0800

    added script8.js

---
+++ js.js/script8.js
@@ -0,0 +1,8 @@
+let mySchool = {name: "UST", location: "Manila", population: "40000"}
+function offer(courses){
+    mySchool = 'UST';
+}
+
+function bind('mySchool'){
+    console.log('UST offers architecture, nursing, information systems, ....')
+}
