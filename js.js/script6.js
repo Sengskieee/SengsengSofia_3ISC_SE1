00c19af2e28e749a0e3d1d2bff2370dbd4249a20
Sengskieee <sofiaallyson.sengseng.iics@ust.edu.ph>
2021-09-14 09:30:55 +0800

    added script6.js

---
+++ js.js/script6.js
@@ -0,0 +1,18 @@
+class Cinema
+{
+    function show(name, movie){
+        return{
+            name = 'Cinema 1',
+            movie = 'End Game',
+            introduce: function(){
+                console.log(name);
+                console.log(movie);
+            }
+        }
+    }
+}
+
+
+
+
+
